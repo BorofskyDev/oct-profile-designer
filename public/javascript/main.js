8272818b6c44d8c0c31d59375ed48481ee96b833
solovyovsdom <joelborofsky@mac.com>
2022-10-21 15:02:04 -0500

    completed works

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,5 +3,7 @@ import '../styles/navbar.css'
 import '../styles/hero.css'
 import '../styles/stack.css'
 import '../styles/about.css'
+import '../styles/icons.css'
+import '../styles/works.css'
 
 import './navbar.js'
